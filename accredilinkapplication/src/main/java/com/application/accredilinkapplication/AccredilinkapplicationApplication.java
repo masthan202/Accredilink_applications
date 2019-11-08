424cb83b3704c20078a888c346ba5d2c032e6dd1
Masthan <54172137+masthan202@users.noreply.github.com>
2019-11-08 17:17:56 +0530

    BaseVersion
    Project Structure

---
+++ accredilinkapplication/src/main/java/com/application/accredilinkapplication/AccredilinkapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.application.accredilinkapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccredilinkapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccredilinkapplicationApplication.class, args);
+	}
+
+}
---
+++ accredilinkapplication/src/test/java/com/application/accredilinkapplication/AccredilinkapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.accredilinkapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccredilinkapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
